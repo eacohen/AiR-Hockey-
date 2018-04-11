32e8456dd605af7ca146a5b5d03bbf04dc578e2e
Eli Cohen <eacohen@andrew.cmu.edu>
2018-04-10 21:20:49 -0400

    faster puck

--- game.py
+++ game.py
@@ -132,7 +132,7 @@ class Game:
         pygame.init()
 
         self.arena = Arena()
-        self.puck = Puck(Vector(60, 60), Vector.polar(1000, pi/6), 50)
+        self.puck = Puck(Vector(60, 60), Vector.polar(3000, pi/6), 50)
         self.paddle_1 = Paddle(Vector(300, 300), 70, (65, 5, 5))
         self.paddle_2 = Paddle(Vector(200, self.arena.y_len / 2), 70, (65, 5, 5))
         self.clock = pygame.time.Clock()
